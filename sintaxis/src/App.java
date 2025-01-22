d8dc8dabb58ee6b89e021cb147cfdf288b6f9376
Saúl Alejandro Maldonado Ladrillero <123046350@upq.edu.mx>
2025-01-22 15:36:09 -0600

    Finalizamos practica No2 Sintxis

--- sintaxis/src/App.java
+++ sintaxis/src/App.java
@@ -1,5 +1,93 @@
+//IMPORTACIONES
+
+//import java.util.Random;
 public class App {
+
+    /*Comentario
+     * de varias
+     * lineas
+     */
+
+    //Metodo Main
     public static void main(String[] args) throws Exception {
+
+        //1.Cadenas
+        /* 
         System.out.println("Hola mi nombre es Saúl Alejandro");
+        String cadenas = "ivan" + "guerra" + "lopez";
+
+        
+        System.out.println(cadenas.length());
+        System.out.println(cadenas.substring(2,5));
+        System.out.println(cadenas.substring(beginIndex:2));
+        System.out.println(cadenas.substring(0,5));
+        */
+
+        /*3. Variables 
+
+        int x = 5, x2;
+        double  y = 1.25, y2;
+        String z = "grupo204", z1;
+
+
+        x2 = x;
+        z1 = String.valueOf(y);
+        double asd = Double.valueOf(x).doubleValue();
+
+    System.out.println(x2);
+    System.out.println(z1);
+    System.out.println(asd);
+
+    //Creamos un objeto rnd random
+    Random rnd = new Random();
+
+    //Generamos numeros y lo guardamos 
+    int numAleatorio = rnd.nextInt();
+    double numdouble = rnd.nextDouble();
+    System.out.println("Aleatorio Entero" +numAleatorio);
+    System.out.println("Aleatorio Double" +numdouble);
+     */
+
+     /*4.Solicitud de datos 
+
+     Scanner scn = new Scanner(System.in);
+
+     System.out.println("Introduce cualquier dato: ");
+     String dato = scn.nextLine();
+
+     System.out.println("Ingresa cualquier dato entero:");
+     int datoEntero = scn.nextInt();
+
+     System.out.println("Introduce cuaquier dato decimal:");
+     double datoDecimal = scn.nextDouble();
+
+     System.out.println("Introduce cualquier dato:" + dato);
+     System.out.println("Introduce cualquier dato entero" + datoEntero);
+     System.out.println("Introduce cualquier dato decimal" + datoDecimal);
+      */
+
+      /*5. Boolean, operadores logicos y de comparación 
+
+      System.out.println(10 > 9);
+      System.out.println(10 == 9);
+      System.out.println(10 < 9);
+      System.out.println(10 >= 9);
+      System.out.println(10 <= 9);
+      System.out.println(10 != 9);
+     */
+
+      int x = 3;
+      System.out.println(x < 5 && x>10);
+      System.out.println(x < 5 || x>10);
+      System.out.println(!(x < 5 && x>10));
+
+
+
+
+
+    
+
+
+
     }
 }
