9bd43a4334d913efdc7d6e9fca0bce902c11cdce
Saúl Alejandro Maldonado Ladrillero <123046350@upq.edu.mx>
2025-01-27 15:07:40 -0600

    Terminación del 5to ejercicio
    Se concluyo con el ejercicio 5 antes de los anteriores a exclusión del primero 2/6

--- sintaxis/src/practica3.java
+++ sintaxis/src/practica3.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class practica3 {
     
     public static void main(String[] args) {
-        
+
          Scanner scn = new Scanner(System.in);
+
+         /*Programa #1
 
          System.out.println("Ingrese numero de horas trabajadas: ");
          int horasTrabajadas = scn.nextInt();
@@ -12,7 +14,36 @@ public class practica3 {
          System.out.println("Ingrese coste por hora tarbajada: ");
          double costeHora = scn.nextDouble();
 
-         System.out.println("Usted cobrara " + horasTrabajadas * costeHora);
+         System.out.println("Usted cobrara: " + horasTrabajadas * costeHora);
+
+          */
+
+         /*Programa #2
+
+         System.out.println("Ingrese sus primeros dos nombres: ");
+         String nombres = scn.nextLine();
+
+         System.out.println("Ingrese su apellido paterno: ");
+         String apellidoPaterno = scn.nextLine();
+
+         System.out.println("Ingrese su apellido materno: ");
+         String apellidoMaterno = scn.nextLine();
+          */
+
+          //Prigrama #5
+
+          int payaso, muñeca;
+          payaso = 112;
+          muñeca = 75;
+
+          System.out.println("Ingrese numero de payasos por paquete: ");
+          int numPayasos = scn.nextInt();
+
+          System.out.println("Ingresa el numero de muñecas por paquete: ");
+          int numMuñecas = scn.nextInt();
+
+          int pesoTotal = (numPayasos * payaso) + (numMuñecas * muñeca);
+          System.out.println("El total de peso del paquete es de: " + pesoTotal + " gramos");
 
     }
 }
