826f5d8c8c046ec6f368711b64210224517b0c4c
Saúl Alejandro Maldonado Ladrillero <123046350@upq.edu.mx>
2025-01-28 16:23:33 -0600

    Finalización del ejercicio #2
    Se finaliza el segundo ejercicio de la practica #4

---
+++ Ejercicio1/src/Ejercicio2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce un número entero: ");
+        int number = scanner.nextInt();
+        
+        if (number % 2 == 0) {
+            System.out.println("El número es par");
+        } else {
+            System.out.println("El número es impar");
+        }
+        
+    }
+
+}
